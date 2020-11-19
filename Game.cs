c1b832b1ad0570dce10c3e1aa335685dc8c34787
Patryk <frogy194@interia.pl>
2020-11-19 16:18:12 +0100

    commit on master

---
+++ Game.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Hangman
+ {
+public static class Game
+{
+    public static Score newGame() {
+        Score wynik = new Score();
+
+    
+
+        Console.WriteLine("test game console");
+        return wynik;
+    }
+    
+}
+ }
---
+++ Score.cs
@@ -0,0 +1,32 @@
+
+
+namespace Hangman
+ {
+public class Score
+    {
+    string playerNick;
+    string dateOfGame;
+    int timeOfGame;
+    int lifes;
+    public void setScore (string pn, string dog, int tog, int l) {
+        playerNick = pn;
+        dateOfGame = dog;
+        timeOfGame = tog;
+        lifes = l;
+    }
+
+    public string getName() {
+        return playerNick;
+    }
+    public string getDate() {
+        return dateOfGame;
+    }
+    public int getTime() {
+        return timeOfGame;
+    }
+    public int etLife() {
+        return lifes;
+    }
+
+    }
+ }
